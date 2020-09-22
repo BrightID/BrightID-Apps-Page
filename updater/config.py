a982fb1771d8bdabe30b2fa6decc12931505a7ec
Siftal <m.khanmohamadzadeh@gmail.com>
2020-09-22 20:25:01 +0330

    read token address from config

--- updater/config.py
+++ updater/config.py
@@ -3,3 +3,4 @@ spreadsheet_id = ''
 scopes = ['']
 apps_url = 'http://node.brightid.org/brightid/v5/apps'
 data_file_addr = ''
+token_file_addr = ''
--- updater/update.py
+++ updater/update.py
@@ -14,7 +14,7 @@ import time
 def read_googl_sheet():
     creds = None
     if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+        with open(config.token_file_addr, 'rb') as token:
             creds = pickle.load(token)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -23,7 +23,7 @@ def read_googl_sheet():
             flow = InstalledAppFlow.from_client_secrets_file(
                 config.credentials_file, config.scopes)
             creds = flow.run_local_server(port=0)
-        with open('token.pickle', 'wb') as token:
+        with open(config.token_file_addr, 'wb') as token:
             pickle.dump(creds, token)
     service = build('sheets', 'v4', credentials=creds)
     sheet = service.spreadsheets()
